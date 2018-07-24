53b8d4001336565c0b117812db79fd1a6be02683
dumbfly <mstepanov@gmail.com>
2018-07-24 17:58:03 +0300

    batch files added

---
+++ va.bat
@@ -0,0 +1,4 @@
+@echo off
+title virtual_analizator
+SET PY_HOME=C:\Users\Administrator\AppData\Local\Programs\Python\Python36
+%PY_HOME%/python.exe main.py
---
+++ watch_dog.bat
@@ -0,0 +1,4 @@
+@echo off
+title watch_dog
+SET PY_HOME=C:\Users\Administrator\AppData\Local\Programs\Python\Python36
+%PY_HOME%/python.exe watch_dog.py
